2c31450bbc7946f6a4bb92861d81e8aed1303590
camilo09m <c.nanco01@ufromail.cl>
2021-09-13 14:22:49 -0300

    hola

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("ingrese una opcion");
+        String hola = teclado.next();
     }
 }
